9cdf67f2742063d771971a80fc6583999236c787
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-07-05 17:15:03 -0300

    Conseguindo finalizar o requisito 3 com a ajuda de Marcos Oliveira.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,72 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 // import { getUser } from '../services/userAPI';
 // import Loading from '../pages/Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.createUser();
+  }
+
+  createUser = async () => {
+    this.setState({
+      loading: true,
+    });
+    const xablau = await getUser();
+    this.setState({
+      userName: xablau.name,
+      loading: false,
+    });
+  }
+
   render() {
+    const {
+      userName,
+      loading,
+    } = this.state;
     return (
       <header data-testid="header-component">
-        {/* <span data-testid="header-user-name" /> */}
-        <nav>
-          <NavLink
-            to="/search"
-            activeClassName="selected"
-            data-testid="link-to-search"
-          >
-            Localizar
-          </NavLink>
-          <NavLink
-            to="/favorites"
-            activeClassName="selected"
-            data-testid="link-to-favorites"
-          >
-            Favoritos
-          </NavLink>
-          <NavLink
-            to="/profile"
-            activeClassName="selected"
-            data-testid="link-to-profile"
-          >
-            Perfil
-          </NavLink>
-        </nav>
+        { loading ? <Loading />
+          : (
+            <div>
+              <span data-testid="header-user-name">
+                { userName }
+              </span>
+              <nav>
+                <NavLink
+                  to="/search"
+                  activeClassName="selected"
+                  data-testid="link-to-search"
+                >
+                  Localizar
+                </NavLink>
+                <NavLink
+                  to="/favorites"
+                  activeClassName="selected"
+                  data-testid="link-to-favorites"
+                >
+                  Favoritos
+                </NavLink>
+                <NavLink
+                  to="/profile"
+                  activeClassName="selected"
+                  data-testid="link-to-profile"
+                >
+                  Perfil
+                </NavLink>
+              </nav>
+            </div>
+          ) }
       </header>
     );
   }
